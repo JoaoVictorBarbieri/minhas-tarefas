5012408233fdedaa0f4b1ba69bad618afbe25ea1
João <joaov.b.euwest@gmail.com>
2024-11-06 16:06:59 -0300

    #componentização e atributos tarefas

--- src/containers/ListaDeTarefas/ListaDeTarefas.tsx
+++ src/containers/ListaDeTarefas/ListaDeTarefas.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../styles/components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver aula',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Vence amanha',
+    prioridade: 'urgente',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino A, B',
+    prioridade: 'importante',
+    status: 'concluída'
+  }
+]
+
 const ListadeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
