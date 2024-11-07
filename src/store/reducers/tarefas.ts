5977254a18bd52d0eee2cfc45b9eb2de4c3bf255
João <joaov.b.euwest@gmail.com>
2024-11-07 16:52:57 -0300

    #implementacao_botao_remover

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -2,36 +2,42 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Tarefa from '../../models/Tarefa'
 import * as enums from '../../utils/enums/Tarefa'
 
+type TarefaState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefaState = {
+  itens: [
+    {
+      id: 1,
+      titulo: 'Estudar JavaScript',
+      descricao: 'Estudar os ultimos módulos',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA
+    },
+    {
+      id: 2,
+      titulo: 'Estudar VueJS',
+      descricao: 'Entender os principais conceitos',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE
+    },
+    {
+      id: 3,
+      titulo: 'Estudar React',
+      descricao: 'Estudar para a prova',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.CONCLUIDA
+    }
+  ]
+}
+
 const tarefasSlice = createSlice({
   name: 'tarefas',
-  initialState: [
-    new Tarefa(
-      'Estudar JavaScript',
-      enums.Prioridade.IMPORTANTE,
-      enums.Status.PENDENTE,
-      '',
-      1
-    ),
-
-    new Tarefa(
-      'Estudar React',
-      enums.Prioridade.URGENTE,
-      enums.Status.CONCLUIDA,
-      'Rever aula 1 do módulo',
-      2
-    ),
-
-    new Tarefa(
-      'Estudar VueJs',
-      enums.Prioridade.URGENTE,
-      enums.Status.PENDENTE,
-      'Praticar o useEffect',
-      3
-    )
-  ],
+  initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state = state.filter((tarefa) => tarefa.id !== action.payload)
+      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
     }
   }
 })
