ce6769d69ebd850526688a2aa33df4a7fcd9bb9f
João <joaov.b.euwest@gmail.com>
2024-11-25 14:51:04 -0300

    #adicionando_pagina

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListadeTarefas from '../../containers/ListaDeTarefas/ListaDeTarefas'
+import BotaoAdicionar from '../../styles/components/BotaoAdicionar'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListadeTarefas />
+    <BotaoAdicionar />
+  </>
+)
+
+export default Home
